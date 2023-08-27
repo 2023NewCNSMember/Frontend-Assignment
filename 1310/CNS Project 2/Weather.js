b1228f027f4ea04d3717c5733865ed31c6fcf1af
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2023-08-27 22:21:23 +0900

    1310박시현 과제 제출
    과제 1
    노션에서의 반응형을 빼고 하였습니다.
    
    과제 2
    기량이 부족하여 현재만

--- 1310/CNS Project 2/Weather.js
+++ 1310/CNS Project 2/Weather.js
@@ -1,4 +1,4 @@
-const apiKey = '6cb08faa165808cef5f64335f15272fc';
+const apiKey = "6cb08faa165808cef5f64335f15272fc";
 
 fetch(
   `http://api.openweathermap.org/geo/1.0/direct?q=Daegu&limit=5&appid=${apiKey}`
@@ -11,5 +11,27 @@ fetch(
       `http://api.openweathermap.org/data/2.5/forecast?lat=${geo[0].lat}&lon=${geo[0].lon}&appid=${apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+
+        // Value of object
+        const idWeather = document.getElementById("weather");
+        const idCityName = document.getElementById("cityName");
+        const idTempMin = document.getElementById("tempMin");
+        const idTempMax = document.getElementById("tempMax");
+        const idWindSpeed = document.getElementById("windSpeed");
+
+        idCityName.textContent =
+          JSON.stringify(data.city.name);
+        idWeather.textContent =
+          JSON.stringify(data.list[39].weather[0].main);
+        idTempMin.textContent =
+          "최저 온도 : " +
+          Math.round(JSON.stringify(data.list[39].main.temp_min) - 273.15);
+        idTempMax.textContent =
+          "최고 온도 : " +
+          Math.round(JSON.stringify(data.list[39].main.temp_max) - 273.15);
+        idWindSpeed.textContent =
+          "풍속 : " + JSON.stringify(data.list[39].wind.speed);
+      });
   });
