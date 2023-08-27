8040a4748f8ec0634255d1e3ef00921a70e8ff27
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2023-08-27 18:59:20 +0900

    1413 박상민 과제제출
    date.js 파일엔 현재 연도와, 월,일 그리고 오전 오후를 구분하여 현재 시간을 출력하는 코드를 짰고, index.html 파일엔 전체적인 뼈대 구성을 하였고, main.js엔 버튼 동작 및 요일 버튼을 누르면 그 요일의 날씨, 강수 확률, 최저와 최고 온도를 출력하게 하였습니다. style.css에선 텍스트의 전체적인 위치 조정과 요일 버튼을 살짝 둥글게 만드는데 사용하였습니다.

---
+++ main.js
@@ -0,0 +1,50 @@
+const apiKey = '88696efc50f0ce92ae2a33fec6ab7645';
+const city = 'Daegu';
+
+const mondayBtn = document.getElementById('mon');
+const tuesdayBtn = document.getElementById('tues');
+const wednesdayBtn = document.getElementById('wednes');
+const thursdayBtn = document.getElementById('thurs');
+const fridayBtn = document.getElementById('fri');
+
+mondayBtn.addEventListener('click', () => fetchWeatherForDay('Monday'));
+tuesdayBtn.addEventListener('click', () => fetchWeatherForDay('Tuesday'));
+wednesdayBtn.addEventListener('click', () => fetchWeatherForDay('Wednesday'));
+thursdayBtn.addEventListener('click', () => fetchWeatherForDay('Thursday'));
+fridayBtn.addEventListener('click', () => fetchWeatherForDay('Friday'));
+
+function fetchWeatherForDay(day) {
+    const weatherContainer = document.getElementById('weather-data');
+    weatherContainer.innerHTML = `<h2>${day} 날씨</h2>`;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const filteredData = data.list.filter(item => {
+                const dateTime = new Date(item.dt * 1000);
+                const dayOfWeek = dateTime.toLocaleDateString('en-US', { weekday: 'long' });
+                return dayOfWeek === day;
+            });
+
+            if (filteredData.length > 0) {
+                const weatherInfo = filteredData[0];
+                const description = weatherInfo.weather[0].description;
+                const rainProbability = (weatherInfo.pop * 100).toFixed(0); // Convert to percentage without decimals
+                const minTemperature = weatherInfo.main.temp_min;
+                const maxTemperature = weatherInfo.main.temp_max;
+
+                weatherContainer.insertAdjacentHTML('beforeend', `<p>날씨 : ${description}</p>`);
+                weatherContainer.insertAdjacentHTML('beforeend', `<p>최저 온도 : ${minTemperature.toFixed(2)}°C</p>`);
+                weatherContainer.insertAdjacentHTML('beforeend', `<p>최고 온도 : ${maxTemperature.toFixed(2)}°C</p>`);
+                weatherContainer.insertAdjacentHTML('beforeend', `<p>강수 확률 : ${rainProbability}%</p>`);
+            } else {
+                weatherContainer.textContent = '해당 날짜의 날씨 정보를 찾을 수 없습니다.';
+            }
+        })
+        .catch(error => {
+            console.error('날씨 정보를 가져오는 동안 오류 발생:', error);
+            weatherContainer.textContent = '날씨 정보를 불러오지 못했습니다.';
+        });
+}
